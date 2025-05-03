ef9c16c5564f2001aa8de0f8c7bc5a05a2b13eee
BrielBriel <ruanogabriel20@gmail.com>
2025-05-02 23:55:10 -0500

    Update Program.cs
    xddddddd

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace GestionContenedores
         [STAThread]
         static void Main()
         {
+            Console.WriteLn("victorMarika");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
