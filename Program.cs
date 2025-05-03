44578a3c0a7f77a430642108f31740a25156e090
Alexander <thevector1999@gmail.com>
2025-05-02 23:50:44 -0500

    ejemplo commit

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace GestionContenedores
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("w");
         }
     }
 }
